5af4234616210a840a0ffe552af9f4bddd1714c3
Miguel Nieto <miguelnieto1828@gmail.com>
2023-08-30 19:34:38 -0500

    first commit

---
+++ Generadordecontras.py
@@ -0,0 +1,18 @@
+import random 
+from werkzeug.security import generate_password_hash
+
+minus ="abcdefghijklmnopqrstuvwxyz"
+mayus = minus.upper()
+numeros = "0123456789"
+simbolos = "@()[]{}*,/-_¿?.¡!$<#>6+%="
+
+base = minus+mayus+numeros+simbolos
+longitud = 12
+
+for _ in range(10):
+
+    muestra = random.sample(base,longitud)
+    password = "".join(muestra)
+    password_encriptado = generate_password_hash(password) 
+    print("{}  {}".format(password, password_encriptado))
+
